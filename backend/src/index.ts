cf1d20fc8556233ffcdc6d29ead39305b07bec88
Ayush <ayushgevariya8914@gmail.com>
2026-03-01 22:05:47 +0530

    chore: Prisma and Connection Pooling config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
+
+
+app.get("/", (c) => c.text("Hello from Hono!"));
 
 export default app
